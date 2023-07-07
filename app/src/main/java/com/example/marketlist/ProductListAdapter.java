8076a829c28cb6c332259daedb3f8723a6a74c32
Aline Souza <alinesouza.hrizon@gmail.com>
2023-07-07 20:56:59 -0300

    feat(ProductListAdapter): Adiciona função de deletação

--- app/src/main/java/com/example/marketlist/ProductListAdapter.java
+++ app/src/main/java/com/example/marketlist/ProductListAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +13,28 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ViewHolder> {
-
     private ArrayList<String> itensList;
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements com.example.marketlist.ViewHolder {
         private final CheckBox checkBox;
         private final TextView textView;
+        private final ImageView delete;
         ViewHolder(View itemView) {
             super(itemView);
 
             checkBox = (CheckBox) itemView.findViewById(R.id.cb_item);
             textView = (TextView) itemView.findViewById(R.id.name_product);
+            delete = (ImageView) itemView.findViewById(R.id.delete_view);
         }
-
         public CheckBox getCheckBox() {
             return checkBox;
         }
 
         public TextView getTextView() {
             return textView;
+        }
+
+        public ImageView getDelete() {
+            return delete;
         }
     }
 
@@ -47,6 +52,14 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
     @Override
     public void onBindViewHolder(@NonNull ProductListAdapter.ViewHolder holder, int position) {
         holder.getTextView().setText(itensList.get(position));
+        holder.getDelete().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    itensList.remove(holder.getAdapterPosition());
+
+                    notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
