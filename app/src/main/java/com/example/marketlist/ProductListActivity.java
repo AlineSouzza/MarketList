8f99fe230d70c2058891f0c048ddec70fb1c6cf5
Aline Souza <alinesouza.hrizon@gmail.com>
2023-07-07 20:56:09 -0300

    feat(ProductListActivity): Dinaminiza a lista de produtos

--- app/src/main/java/com/example/marketlist/ProductListActivity.java
+++ app/src/main/java/com/example/marketlist/ProductListActivity.java
@@ -1,6 +1,9 @@
 package com.example.marketlist;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,21 +13,36 @@ import java.util.ArrayList;
 
 public class ProductListActivity extends AppCompatActivity {
 
+    private ArrayList<String> itensList;
+    private ProductListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        ImageView imageView = findViewById(R.id.image_view);
+        EditText editText = findViewById(R.id.edit_text);
 
-        ArrayList<String> itensList = new ArrayList<String>();
-        itensList.add("Feijão");
-        itensList.add("Feijão");
-        itensList.add("Feijão");
-        itensList.add("Feijão");
-        itensList.add("Feijão");
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String novoItem = editText.getText().toString();
 
-        ProductListAdapter adapter = new ProductListAdapter(itensList);
+                if (novoItem.isEmpty()) return;
+
+                itensList.add(novoItem);
+
+                editText.getText().clear();
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        itensList = new ArrayList<String>();
+
+        adapter = new ProductListAdapter(itensList);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(llm);
