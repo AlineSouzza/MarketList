94073e190072f7261baf41034f941f19bbe0c5c4
Aline Souza <alinesouza.hrizon@gmail.com>
2023-07-19 14:55:52 -0300

    feat(ProductListAdapter): Implementa dialog para alimentação da lista de produtos. Implementa função de edição e deletação

--- app/src/main/java/com/example/marketlist/ProductListAdapter.java
+++ app/src/main/java/com/example/marketlist/ProductListAdapter.java
@@ -3,28 +3,39 @@ package com.example.marketlist;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ViewHolder> {
-    private ArrayList<String> itensList;
-    public static class ViewHolder extends RecyclerView.ViewHolder implements com.example.marketlist.ViewHolder {
+    private final ArrayList<String> itensList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         private final CheckBox checkBox;
         private final TextView textView;
         private final ImageView delete;
+        private final ImageView edit;
+        private final EditText editText;
+
+        private ProductListActivity activity = new ProductListActivity();
         ViewHolder(View itemView) {
             super(itemView);
 
             checkBox = (CheckBox) itemView.findViewById(R.id.cb_item);
             textView = (TextView) itemView.findViewById(R.id.name_product);
             delete = (ImageView) itemView.findViewById(R.id.delete_view);
+            edit = (ImageView) itemView.findViewById(R.id.edit_view);
+            editText = (EditText) itemView.findViewById(R.id.edit_item);
         }
+
         public CheckBox getCheckBox() {
             return checkBox;
         }
@@ -35,6 +46,13 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
 
         public ImageView getDelete() {
             return delete;
+        }
+
+        public ImageView getEdit() {
+            return edit;
+        }
+        public EditText getEditText() {
+            return editText;
         }
     }
 
@@ -55,9 +73,59 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
         holder.getDelete().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    itensList.remove(holder.getAdapterPosition());
+                itensList.remove(holder.getAdapterPosition());
 
-                    notifyDataSetChanged();
+                notifyDataSetChanged();
+            }
+        });
+
+        holder.getEdit().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itensList.get(holder.getAdapterPosition());
+
+                Button saveButton;
+                Button cancelButton;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+
+                builder.setTitle("Edição");
+                final View customLayout
+                        = LayoutInflater.from(v.getContext()).inflate(
+                        R.layout.dialog_edit_item,
+                        null);
+
+                builder.setView(customLayout);
+
+                cancelButton = customLayout.findViewById(R.id.cancel_btn);
+                saveButton = customLayout.findViewById(R.id.save_button);
+                EditText editText = customLayout.findViewById(R.id.edit_item);
+
+                AlertDialog dialog = builder.create();
+
+                editText.setText(itensList.get(holder.getAdapterPosition()));
+                dialog.show();
+
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.cancel();
+                    }
+                });
+
+                saveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String editItem = editText.getText().toString();
+                        itensList.set(holder.getAdapterPosition(), editItem);
+
+                        if (editItem.isEmpty()) return;
+
+                        dialog.dismiss();
+
+                        notifyDataSetChanged();
+                    }
+                });
             }
         });
     }
